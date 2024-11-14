6a779b72db09e91263691bea070ec69c7aba9dc9
luizpaulo73 <luiz.paulo2005@hotmail.com>
2024-11-14 09:23:36 -0300

    Pagina de iniciar trajeto

--- verdi/src/app/conta/page.tsx
+++ verdi/src/app/conta/page.tsx
@@ -29,12 +29,24 @@ export default async function Conta() {
   return (
     <section>
       <h1 className='text-creme text-center text-2xl md:text-3xl'>
-        Bem vindo(a) {primeiroNome}
+        Olá {primeiroNome}!
       </h1>
       <div className='border-2 border-green-700 w-[90vw] p-2 mt-8 sm:w-96 xl:w-[33vw] flex flex-col gap-5'>
-        <p className='text-creme lg:text-xl'>Quilômetros Percorridos - {quilometrosPercorridos}</p>
-        <p className='text-creme lg:text-xl'>Pontos Conquistados - {pontosConquistados}</p>
-        <p className='text-creme lg:text-xl'>Créditos - {creditos}</p>
+        <div className='flex justify-between'>
+          <p className='text-creme lg:text-xl'>Quilômetros Percorridos - </p>
+          <p className='text-creme lg:text-xl'>{quilometrosPercorridos} Km</p>
+        </div>
+        <div className='flex justify-between'>
+          <p className='text-creme lg:text-xl'>Pontos Conquistados - </p>
+          <p className='text-creme lg:text-xl'>{pontosConquistados}</p>
+          </div>
+        <div className='flex justify-between'>
+          <p className='text-creme lg:text-xl'>Créditos - </p>
+          <p className='text-creme lg:text-xl'>{creditos}</p>
+        </div>
+        
+        
+        
       </div>
       <div className='flex justify-around mt-5'>
         <Link href={"/trajeto/iniciar"} className='bg-green-700 text-creme p-2 lg:text-xl'>Iniciar Trajeto</Link>
--- verdi/src/app/trajeto/iniciar/page.tsx
+++ verdi/src/app/trajeto/iniciar/page.tsx
@@ -1,7 +1,73 @@
-import React from 'react'
+"use client"
+import { useState } from "react";
 
-export default function page() {
+export default function IniciarTrajeto() {
+
+  const [origem, setOrigem] = useState<string>("")
+  const [destino, setDestino] = useState<string>("")
+  const [distancia, setDistancia] = useState<string>("")
+  const [meioDeTransporte, setMeioDeTransporte] = useState<string>("")
+
+  const iniciarTrajeto = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    
+
+    try {
+      const response = await fetch(`http://localhost:8080/trajeto/registrar`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ origem, destino, meioDeTransporte }),
+      });
+      // if (response.ok) {
+      //   console.log("Usuário Cadastrado com sucesso");
+      //   setNome("");
+      //   setCpf("");
+      //   setEmail("");
+      //   setSenha("");
+      //   setPlanos("");
+      // } else {
+      //   console.error("Erro ao cadastrar");
+      // }
+    } catch (error) {
+      console.error("Falha no carregamento", error);
+    }
+  };
+
+
+
   return (
-    <div>page</div>
+    <div>
+      <form onSubmit={iniciarTrajeto}>
+        <input 
+        type="text"
+        placeholder="inicio"
+        value={origem}
+        onChange={(e) => setOrigem(e.target.value)}
+        />
+        <p className="text-creme">{distancia}</p>
+        <select
+        name="meioDeTransporte"
+        id="meioDeTransporte"
+        value={meioDeTransporte}
+        onChange={(e) => setMeioDeTransporte(e.target.value)}
+        
+        >
+          <optgroup>
+            <option value="andar">À pé</option>
+            <option value="bicicleta">Bicicleta</option>
+            <option value="metroTrem">Metro / Trem</option>
+            <option value="onibus">Onibus</option>
+          </optgroup>
+        </select>
+        <input
+        type="text"
+        placeholder="fim"
+        value={destino}
+        onChange={(e) => setDestino(e.target.value)}
+        />
+        <button type="submit" className="text-creme">Iniciar</button>
+      </form>
+    </div>
   )
 }
