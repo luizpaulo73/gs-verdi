857a5f4f8671d2ffb546751f0c8b6ddba62b03fb
luizpaulo73 <luiz.paulo2005@hotmail.com>
2024-11-14 10:59:49 -0300

    api do maps adicionada

--- verdi/src/app/trajeto/iniciar/page.tsx
+++ verdi/src/app/trajeto/iniciar/page.tsx
@@ -1,83 +1,18 @@
-"use client";
-import { useState } from "react";
+import Trajeto from "@/components/Trajeto/Trajeto";
+import { getServerSession } from "next-auth";
+import { getUserInfo } from "@/app/api/usuario/[email]/route";
 
-export default function IniciarTrajeto() {
-  const [origem, setOrigem] = useState<string>("");
-  const [destino, setDestino] = useState<string>("");
-  const [distancia, setDistancia] = useState<string>("");
-  const [meioDeTransporte, setMeioDeTransporte] = useState<string>("");
+export default async function IniciarTrajeto() {
+  
+  const session = await getServerSession();
+  const email = session?.user?.email || "";
+  const userInfo = await getUserInfo(email);
+  const idPessoa = userInfo?.id
 
-  const iniciarTrajeto = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const pessoa_id = 37; // Substitua por um valor dinâmico se necessário
-    const meioDeTransporte = "metro";
-
-    console.log("Origem:", origem);
-    console.log("Destino:", destino);
-    console.log("Meio de Transporte:", meioDeTransporte);
-    console.log("Pessoa ID:", pessoa_id);
-
-    if (!pessoa_id || !origem || !destino || !meioDeTransporte) {
-      console.error("Todos os campos são obrigatórios");
-      return;
-    }
-
-    try {
-      const response = await fetch(`http://localhost:5000/registrar`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          pessoa_id,
-          origem,
-          destino,
-          meioDeTransporte,
-        }),
-      });
-
-      if (response.ok) {
-        const data = await response.json();
-        console.log("Trajeto registrado com sucesso:", data);
-        setDistancia(`${data.distanciaKm} km`);
-      } else {
-        const errorData = await response.json();
-        console.error("Erro ao registrar o trajeto:", errorData);
-      }
-    } catch (error) {
-      console.error("Falha no carregamento", error);
-    }
-  };
-
+  console.log(session)
   return (
-    <div>
-      <form onSubmit={iniciarTrajeto}>
-        <input
-          type="text"
-          placeholder="Início"
-          value={origem}
-          onChange={(e) => setOrigem(e.target.value)}
-        />
-        <p className="text-creme">{distancia}</p>
-        <select
-          name="meioDeTransporte"
-          id="meioDeTransporte"
-          value={meioDeTransporte}
-          onChange={(e) => setMeioDeTransporte(e.target.value)}
-        >
-          <option value="andar">À pé</option>
-          <option value="bicicleta">Bicicleta</option>
-          <option value="metroTrem">Metrô / Trem</option>
-          <option value="onibus">Ônibus</option>
-        </select>
-        <input
-          type="text"
-          placeholder="Fim"
-          value={destino}
-          onChange={(e) => setDestino(e.target.value)}
-        />
-        <button type="submit" className="text-creme">
-          Iniciar
-        </button>
-      </form>
-    </div>
+    <>
+      <Trajeto id={idPessoa}/>
+    </>
   );
 }
---
+++ verdi/src/components/Trajeto/Trajeto.tsx
@@ -0,0 +1,84 @@
+"use client";
+import { useState } from 'react'
+
+export default function Trajeto(props: {id:number}) {
+  
+  const [origem, setOrigem] = useState<string>("");
+  const [destino, setDestino] = useState<string>("");
+  const [distancia, setDistancia] = useState<string>("");
+  const [meioDeTransporte, setMeioDeTransporte] = useState<string>("");
+
+  const iniciarTrajeto = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const pessoa_id = props.id;
+    const meioDeTransporte = "metro";
+
+    console.log("Origem:", origem);
+    console.log("Destino:", destino);
+    console.log("Meio de Transporte:", meioDeTransporte);
+    console.log("Pessoa ID:", pessoa_id);
+
+    if (!pessoa_id || !origem || !destino || !meioDeTransporte) {
+      console.error("Todos os campos são obrigatórios");
+      return;
+    }
+
+    try {
+      const response = await fetch(`http://localhost:5000/registrar`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          pessoa_id,
+          origem,
+          destino,
+          meioDeTransporte,
+        }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log("Trajeto registrado com sucesso:", data);
+        setDistancia(`${Math.round(data.distanciaKm * 100) / 100} km`);
+      } else {
+        const errorData = await response.json();
+        console.error("Erro ao registrar o trajeto:", errorData);
+      }
+    } catch (error) {
+      console.error("Falha no carregamento", error);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={iniciarTrajeto}>
+        <input
+          type="text"
+          placeholder="Início"
+          value={origem}
+          onChange={(e) => setOrigem(e.target.value)}
+        />
+        <p className="text-creme">{distancia}</p>
+        <select
+          name="meioDeTransporte"
+          id="meioDeTransporte"
+          value={meioDeTransporte}
+          onChange={(e) => setMeioDeTransporte(e.target.value)}
+        >
+          <option value="andar">À pé</option>
+          <option value="bicicleta">Bicicleta</option>
+          <option value="metroTrem">Metrô / Trem</option>
+          <option value="onibus">Ônibus</option>
+        </select>
+        <input
+          type="text"
+          placeholder="Fim"
+          value={destino}
+          onChange={(e) => setDestino(e.target.value)}
+        />
+        <button type="submit" className="text-creme">
+          Iniciar
+        </button>
+      </form>
+    </div>
+  );
+}
