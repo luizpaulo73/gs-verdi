7016b1ae2e3362d4e5324b23b9ffde95c53709af
luizpaulo73 <luiz.paulo2005@hotmail.com>
2024-11-15 12:40:45 -0300

    pagina de iniciar trajeto estilizada

--- verdi/src/components/Trajeto/Trajeto.tsx
+++ verdi/src/components/Trajeto/Trajeto.tsx
@@ -6,6 +6,7 @@ export default function Trajeto(props: {id:number}) {
   const [origem, setOrigem] = useState<string>("");
   const [destino, setDestino] = useState<string>("");
   const [distancia, setDistancia] = useState<string>("");
+  const [pontos, setPontos] = useState<string>("");
   const [meioDeTransporte, setMeioDeTransporte] = useState<string>("");
 
   const iniciarTrajeto = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -38,7 +39,8 @@ export default function Trajeto(props: {id:number}) {
       if (response.ok) {
         const data = await response.json();
         console.log("Trajeto registrado com sucesso:", data);
-        setDistancia(`${Math.round(data.distanciaKm * 100) / 100} km`);
+        setDistancia(`${Math.round(data.distanciaKm * 100) / 100} Km`);
+        setPontos(`${Math.floor(data.distanciaKm)} Pontos conquistados`)
       } else {
         const errorData = await response.json();
         console.error("Erro ao registrar o trajeto:", errorData);
@@ -49,20 +51,21 @@ export default function Trajeto(props: {id:number}) {
   };
 
   return (
-    <div>
-      <form onSubmit={iniciarTrajeto}>
+    <section className='md:w-screen flex flex-col items-center'>
+      <form onSubmit={iniciarTrajeto} className='flex flex-col items-center gap-6 container sm:w-1/2'>
         <input
           type="text"
           placeholder="Início"
           value={origem}
           onChange={(e) => setOrigem(e.target.value)}
+          className='w-72 bg-green-700 rounded-full px-2 py-1 text-creme placeholder:text-creme md:w-4/5 lg:text-2xl'
         />
-        <p className="text-creme">{distancia}</p>
         <select
           name="meioDeTransporte"
           id="meioDeTransporte"
           value={meioDeTransporte}
           onChange={(e) => setMeioDeTransporte(e.target.value)}
+          className='w-72 bg-green-700 rounded-full px-2 py-1 text-creme placeholder:text-creme md:w-4/5 lg:text-2xl'
         >
           <option value="andar">À pé</option>
           <option value="bicicleta">Bicicleta</option>
@@ -74,11 +77,16 @@ export default function Trajeto(props: {id:number}) {
           placeholder="Fim"
           value={destino}
           onChange={(e) => setDestino(e.target.value)}
+          className='w-72 bg-green-700 rounded-full px-2 py-1 text-creme placeholder:text-creme md:w-4/5 lg:text-2xl'
         />
-        <button type="submit" className="text-creme">
+        <button type="submit" className="text-creme bg-green-700 p-2 lg:text-2xl  ">
           Iniciar
         </button>
       </form>
-    </div>
+      <div className='mt-5 sm:w-1/3'>
+        {<p className="text-creme text-center lg:text-2xl xl:text-3xl">{distancia}</p>}
+        {<p className='text-creme text-center lg:text-2xl xl:text-3xl'>{pontos}</p>}
+      </div>
+    </section>
   );
 }
