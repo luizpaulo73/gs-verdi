971b4d909e0a19234f0a66f06ce46741149c6665
luizpaulo73 <luiz.paulo2005@hotmail.com>
2024-11-19 09:41:57 -0300

    editar conta com botao de deletar conta adicionado

--- verdi/src/app/conta/editar/page.tsx
+++ verdi/src/app/conta/editar/page.tsx
@@ -4,6 +4,7 @@ import { getUserInfo } from "@/app/api/usuario/[email]/route";
 
 import FormEditarPlano from "@/components/FormEditarPlano/FormEditarPlano";
 import FormEditarSenha from "@/components/FormEditarSenha/FormEditarSenha";
+import BotaoDelete from "@/components/BotaoDelete/BotaoDelete";
 
 export default async function EditarConta() {
 
@@ -19,9 +20,15 @@ export default async function EditarConta() {
     const idUsuario:number = userInfo.id
     const plano = userInfo.planos
      return (
-        <div className="flex flex-col md:flex-row gap-8 justify-around w-full items-center">
-        <FormEditarSenha idUsuario={idUsuario}/>
-        <FormEditarPlano idUsuario={idUsuario} plano={plano} />
+        <div className="flex flex-col gap-8 justify-around w-full items-center min-h-[70vh]">
+          <h1 className="text-center text-creme text-3xl mb-4">Editar Conta</h1>
+          <div className="flex flex-col md:flex-row md:justify-around md:items-center w-full md:w-2/3 py-8 gap-8">
+            <FormEditarSenha idUsuario={idUsuario}/>
+            <FormEditarPlano idUsuario={idUsuario} plano={plano} />
+          </div>
+          <div className="min-w-60 flex flex-col items-center justify-center min-h[40vh] p-2 gap-5 md:w-1/2">
+            <BotaoDelete idUsuario={idUsuario}/>
+          </div>
         </div>
      )
 }
---
+++ verdi/src/components/BotaoDelete/BotaoDelete.tsx
@@ -0,0 +1,65 @@
+    "use client"
+    import { useState } from "react";
+    import { signOut } from "next-auth/react";
+
+    export default function BotaoDelete(props:{idUsuario:number}) {
+
+        const idUsuario = props.idUsuario
+
+        const [telaDeletar, setTelaDeletar] = useState<boolean>(false);
+        const [confirmarDelete, setConfirmarDelete] = useState<string>("");
+
+        const deletarConta = async (e : React.FormEvent<HTMLFormElement>) => {
+            e.preventDefault();
+            if (confirmarDelete.toUpperCase() !== "DELETAR") {
+                alert('Você deve digitar "DELETE" para confirmar a exclusão da conta.');
+                return;
+            }
+            try {
+            const response = await fetch(
+                `http://localhost:8080/pessoas/${idUsuario}`,
+                { method: "DELETE" }
+            );
+            if (response.ok) {
+                signOut()
+            } else {
+                const errorData = await response.json();
+                console.error("Erro ao carregar o trajeto:", errorData);
+            }
+            } catch (error) {
+            console.error("Falha no carregamento", error);
+            }
+        };
+
+    return (
+        <>
+        {telaDeletar === false ? 
+        <div className="w-full flex flex-col items-center">
+        <button onClick={() => setTelaDeletar(!telaDeletar)} className='bg-red-600 text-white p-2'>Deletar Conta</button>
+        <p className="text-white">Essa ação é irreversível</p>
+        </div>
+        :
+        <form onSubmit={deletarConta} className="flex flex-col gap-8">
+            <div className="flex flex-col gap-6">
+                <label htmlFor="deletar" className="text-creme">Digite
+                <span className="text-red-600"> {'"DELETAR"'}</span> para apagar sua conta</label>
+                <input
+                type="text"
+                id="deletar"
+                value={confirmarDelete}
+                onChange={(e) => setConfirmarDelete(e.target.value)}
+                className="border-2 border-red-600 placeholder:text-red-600 text-red-800 rounded-full px-2 md:w-full"/>
+            </div>
+        <div className="flex justify-around">
+            <button
+            type="submit"
+            className="bg-red-600 text-white p-2">Confirmar</button>
+            <button
+            onClick={() => setTelaDeletar(!telaDeletar)}
+            className="bg-green-700 text-white p-2">Cancelar</button>
+        </div>
+        </form>}
+        
+        </>
+    )
+    }
