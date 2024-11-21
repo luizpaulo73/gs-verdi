31389793f62828ae164e0806d3d02f0c9bc0fa9d
luizpaulo73 <luiz.paulo2005@hotmail.com>
2024-11-20 22:59:18 -0300

    Mapa adicionado no historico de trajetos

--- verdi/src/components/ListaTrajeto/ListaTrajeto.tsx
+++ verdi/src/components/ListaTrajeto/ListaTrajeto.tsx
@@ -4,12 +4,14 @@ import { useEffect, useState } from "react";
 import Image from "next/image";
 
 import { TipoTrajeto } from "@/types/tipoData";
-import ping from "@/img/ping.png"
+import ping from "@/img/ping.png";
+import Mapa from "../Mapa/Mapa";
 
 export default function ListaTrajeto(props: { id: number }) {
   const pessoa_id = props.id;
 
   const [trajetos, setTrajetos] = useState<TipoTrajeto[]>([]);
+  const [expandidoId, setExpandidoId] = useState<number | null>(null); // Armazena o ID do trajeto expandido
 
   useEffect(() => {
     const fetchTrajetos = async () => {
@@ -36,22 +38,58 @@ export default function ListaTrajeto(props: { id: number }) {
     fetchTrajetos();
   }, [pessoa_id]);
 
+  const toggleExpandir = (id: number) => {
+    if (expandidoId === id) {
+      setExpandidoId(null);
+    } else {
+      setExpandidoId(id);
+    }
+  };
+
   return (
     <div className="flex flex-col gap-4 mx-2 w-full items-center">
       {trajetos.map((info) => (
-        <div key={info.id} className="border-2 border-green-700 flex flex-col items-center justify-between p-2 gap-5
-                                      sm:w-2/3 lg:flex-row">
+        <div
+          key={info.id}
+          className="border-2 border-green-700 flex flex-col items-center justify-between p-2 gap-5
+                     sm:w-2/3"
+        >
+          <div className="flex justify-between w-full">
           <div>
-          <p className="text-creme text-xs md:text-base xl:text-lg">Início: {info.origem}</p>
-          <Image src={ping} alt="Ping" className="h-10 w-auto"/>
-          <p className="text-creme text-xs md:text-base xl:text-lg">Destino: {info.destino}</p>
+            <p className="text-creme text-xs md:text-base xl:text-lg">
+              Início: {info.origem}
+            </p>
+            <div className="flex">
+              <Image src={ping} alt="Ping" className="h-10 w-auto" />
+              <button
+                onClick={() => toggleExpandir(info.id)}
+                className="ml-2 text-green-500 hover:text-green-300 duration-300"
+              >
+                {expandidoId === info.id ? "Fechar" : "Expandir"}
+              </button>
+            </div>
+            <p className="text-creme text-xs md:text-base xl:text-lg">
+              Destino: {info.destino}
+            </p>
           </div>
           <div className="bg-green-700 h-2"></div>
           <div className="text-center">
-          <p className="text-creme text-xs md:text-base xl:text-lg">Distância: {info.distanciaKm} Km</p>
-          <p className="text-creme text-xs md:text-base xl:text-lg">Meio de transporte: {info.meioDeTransporte}</p>
-          <p className="text-creme text-xs md:text-base xl:text-lg">Pontos conquistados: {info.pontos}</p>
+            <p className="text-creme text-xs md:text-base xl:text-lg">
+              Distância: {info.distanciaKm} Km
+            </p>
+            <p className="text-creme text-xs md:text-base xl:text-lg">
+              Meio de transporte: {info.meioDeTransporte}
+            </p>
+            <p className="text-creme text-xs md:text-base xl:text-lg">
+              Pontos conquistados: {info.pontos}
+            </p>
           </div>
+          </div>
+          {expandidoId === info.id && (
+            <div className="w-full h-[400px]">
+            <Mapa inicio={info.origem} destino={info.destino}/>
+            </div>
+          )}
         </div>
       ))}
     </div>
